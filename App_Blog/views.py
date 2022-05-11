d097d560a3baa264262e82425904fd0b49581642
TANJIM <rtanjim182016@bscse.uiu.ac.bd>
2022-05-11 13:29:10 +0600

    Create_Blog_features added

--- App_Blog/views.py
+++ App_Blog/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def blog_list(request):
     return render(request,'App_Blog/blog_list.html',context={})
+
+
+class CreateBlog(LoginRequiredMixin, CreateView):
+    model = Blog
+    template_name = 'App_Blog/create_blog.html'
+    fields = ('blog_title', 'blog_content', 'blog_image',)
+
+    def form_valid(self, form):
+        blog_obj = form.save(commit=False)
+        blog_obj.author = self.request.user
+        title = blog_obj.blog_title
+        blog_obj.slug = title.replace(" ", "-") + "-" + str(uuid.uuid4())
+        blog_obj.save()
+        return HttpResponseRedirect(reverse('index'))
+
